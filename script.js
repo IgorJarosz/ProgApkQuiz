7c076fe895cb84da4c702bc810895bb9c8afedfd
IgorJarosz <90689664+IgorJarosz@users.noreply.github.com>
2021-11-16 12:09:25 +0100

    Podmiana plików

--- script.js
+++
@@ -1,8 +0,0 @@
-const przycRozp = document.querySelector("#przycRozp");
-
-przycRozp.addEventListener("click", function() {
-  const imieGracza = document.querySelector("#imieGracza").value;
-  sessionStorage.imieGracza = imieGracza;
-  sessionStorage.iloscPkt = 0;
-  window.location = "index2.html";
-});
