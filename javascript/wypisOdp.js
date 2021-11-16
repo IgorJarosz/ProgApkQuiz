1771494eec549fe8a3d1a81df48a21b2f3c8151f
IgorJarosz <90689664+IgorJarosz@users.noreply.github.com>
2021-11-16 12:56:47 +0100

    Uaktualniono odp i pkt
    Uaktualniono strone wyświetlającą odpowiedzi i punkty

---
+++ javascript/wypisOdp.js
@@ -0,0 +1,10 @@
+const miejsceNaIimie = document.querySelector("#tuImiea");
+const miejsceNaPunkty = document.querySelector("#ilePkt");
+
+
+
+miejsceNaIimie.innerHTML += sessionStorage.imieGracza + " twoja ilość punktów to: ";
+
+miejsceNaPunkty.innerHTML = sessionStorage.iloscPkt;
+
+let x = new array.from(sessionStorage.bledneOdp);
